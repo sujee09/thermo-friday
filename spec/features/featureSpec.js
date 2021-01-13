d4658fa32240994cdfe2a1285d62f3e4d29f221e
Sarah Power <sarahjonipower0@gmail.com>
2021-01-13 17:05:49 +0000

    added temp decrease and error past 10degrees

--- spec/features/featureSpec.js
+++ spec/features/featureSpec.js
@@ -1,9 +1,27 @@
 'use strict';
 
 describe('Feature Test', function(){
-  var thermostat = new Thermostat();
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
   it('thermostat temperature can be increased by 1', function(){
     thermostat.increase()
     expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('thermostat temperature can be decrease by 1', function(){
+      thermostat.decrease()
+      expect(thermostat.temperature).toEqual(19);
+  });
+
+  it('temperature cannot go below 10', function(){
+    [1,2,3,4,5,6,7,8,9,10].forEach(function(){
+      thermostat.decrease();
+    });
+     expect(function(){ thermostat.decrease(); }).toThrowError('cannot decrease temperature below 10');
+     expect(thermostat.temperature).toEqual(10)
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,11 +3,18 @@
 class Thermostat {
 
   constructor() {
-    this.temperature = 20
+    this.temperature = 20;
   };
 
   increase() {
     this.temperature++
   };
 
-}
+  decrease() {
+    if(this.temperature === 10) {
+    throw new Error('cannot decrease temperature below 10'); 
+    }
+    this.temperature--
+  };
+
+};
