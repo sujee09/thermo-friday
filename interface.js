ac797d63d1857cb8af09fefb44a942c245764c41
Sujeevan Rajagopalan <rsujee@MA307.local>
2021-01-15 16:19:20 +0000

    API added

--- interface.js
+++ interface.js
@@ -39,7 +39,21 @@ $(document).ready(function() {
           $('#temperature').css('color', 'red')
         }
       }
-      
-    });
 
-    
+    displayWeather('London');
+
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      displayWeather(city);
+    })
+
+      function displayWeather(city) {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+        var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+        var units = '&units=metric';
+        $.get(url + token + units, function(data) {
+          $('#current-temperature').text(data.main.temp);
+        })
+      }
+    });
